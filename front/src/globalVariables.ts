67192317b81ce2878f965079be93303b637bd9bf
DIALLO David <david.diallo@gmail.com>
2021-02-14 23:39:21 +0100

    correct hostname

--- front/src/globalVariables.ts
+++ front/src/globalVariables.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 // we supose that the backend is on the same server. if not, replace this export by a hard coded ip
 export const hostname = document.location.hostname
-export const BASE_API_URL = "http://backend:8000/api"
+export const BASE_API_URL = "http://" + hostname + ":8000/api"
 
 @Injectable()
 export class globalVariables {
